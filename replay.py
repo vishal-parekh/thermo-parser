de6efe53da14e80c1ce879956b32a415cf089351
Vishal Parekh <86313287+vishal-parekh@users.noreply.github.com>
2021-08-04 15:43:17 -0500

    Wrote if condition to handle gzipped jsonlines file

---
+++ replay.py
@@ -0,0 +1,23 @@
+#Small library to open normal or gzipped json lines file(s)
+import json_lines
+
+command = input("Enter Command: ")
+
+#Split up whole command to list elements to store rawpath (as that will be needed to determine how to handle file type in evaluation)
+commandsplit = command.split()
+rawpath = commandsplit[1]
+
+#If statement to evaluate updateTime passed in command to get ambientTemp for that json object,
+#else return error stating ambientTemp not available in json object for that particular updateTime
+
+#Logic to handles gzipped jsonlines file
+if rawpath.endswith('jsonl.gz'):
+     with json_lines.open(rawpath) as path:
+          for obj in path:
+               if "ambientTemp" in obj["update"] and commandsplit[2] in obj["updateTime"]:
+                    ambientTemp = str(obj["update"]["ambientTemp"])
+                    print(ambientTemp)
+                    break
+          else:
+               print("Error:ambientTemp not available in this update")
+
