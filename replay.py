ef706e8a5465c12f896c7bd982d1cb0a47d8d4e3
Vishal Parekh <vishalcparekh@outlook.com>
2023-07-22 20:19:06 -0500

    Refactor error handling with exceptions

--- replay.py
+++ replay.py
@@ -1,33 +1,46 @@
-#Small library to open normal or gzipped json lines file(s)
+# Small library to open normal or gzipped json lines file(s)
+import sys
+
 import json_lines
 
-command = input("Enter Command: ")
 
-#Split up whole command to list elements to store rawpath (as that will be needed to determine how to handle file type in evaluation)
-commandsplit = command.split()
-rawpath = commandsplit[1]
+def find_ambient_temp(input):
+    # Split up whole command to list elements to store rawpath (as that will be needed to determine how to handle file type in evaluation)
+    filepath = input[0]
 
-#If statement to evaluate updateTime passed in command to get ambientTemp for that json object,
-#else return error stating ambientTemp not available in json object for that particular updateTime
+    # If statement to evaluate updateTime passed in command to get ambientTemp for that json object,
+    # else return error stating ambientTemp not available in json object for that particular updateTime
 
-#Logic to handles gzipped jsonlines file
-if rawpath.endswith('jsonl.gz'):
-     with json_lines.open(rawpath) as path:
-          for obj in path:
-               if "ambientTemp" in obj["update"] and commandsplit[2] in obj["updateTime"]:
-                    ambientTemp = str(obj["update"]["ambientTemp"])
-                    print(ambientTemp)
-                    break
-          else:
-               print("Error:ambientTemp not available in this update")
+    # Handle gzipped jsonlines file
+    if filepath.endswith("jsonl.gz"):
+        try:
+            with json_lines.open(filepath) as path:
+                for obj in path:
+                    if obj["updateTime"] == input[1] and "ambientTemp" in obj.get(
+                        "update"
+                    ):
+                        return obj["update"]["ambientTemp"]
+                else:
+                    raise Exception("updateTime does not exist")
+        except Exception as e:
+            print(f"An error occured: {e}")
+            raise e
 
-#Logic to handle normal jsonlines file.
-elif rawpath.endswith('jsonl'):
-     with json_lines.open(rawpath) as path:
-          for obj in path:
-               if "ambientTemp" in obj["update"] and commandsplit[2] in obj["updateTime"]:
-                    ambientTemp = obj["update"]["ambientTemp"]
-                    print(ambientTemp)
-                    break
-          else:
-               print("Error: ambientTemp not available in this update")
+    # Handle normal jsonlines file.
+    if filepath.endswith("jsonl"):
+        try:
+            with json_lines.open(filepath) as path:
+                for obj in path:
+                    if obj["updateTime"] == input[1] and "ambientTemp" in obj.get(
+                        "update"
+                    ):
+                        return obj["update"]["ambientTemp"]
+                else:
+                    raise Exception("updateTime does not exist")
+        except Exception as e:
+            print(f"An error occured: {e}")
+            raise e
+
+
+if __name__ == "__main__":
+    print(find_ambient_temp(sys.argv[1:]))
